4d215251f140c49a5a3ba6724f6a0f6ca3c94a67
Jeremy <jlim0208@student.monash.edu>
2026-05-05 15:34:56 +1000

    Dynamic Thresholds
     - Changes names according to standardise naming conventions to camelCase
     - Adds dynamic thresholds based on distance from peaks
     - Remove unnecessary code

--- main.m
+++ main.m
@@ -2,9 +2,9 @@
 [y, fs] = audioread('02Tchaikovsky_CapriccioItalienOp.45_cut.mp3');
 
 %% Shift fft to 0-center, normalise and use dB scale
-function [output] = fftshiftnormdb(fft)
-    fftshifted = fftshift(abs(fft));
-    output = 20*log10(abs(fftshifted)/max(abs(fftshifted)));
+function [output] = fftshiftNormdB(fft)
+    fftShifted = fftshift(abs(fft));
+    output = 20*log10(abs(fftShifted)/max(abs(fftShifted)));
 end
 
 %% Segment audio
@@ -28,34 +28,34 @@ N1 = length(seg1);
 
 omega1khz = (-floor(N1/2):(N1-1-floor(N1/2)))*(fs/N1)/1000;
 
-figure(1); plot(omega1khz, fftshiftnormdb(fft1));
+figure(1); plot(omega1khz, fftshiftNormdB(fft1));
 title("FFT Magnitude (Segment 1)")
 xlabel("Frequency (kHz)")
 ylabel("|FFT|")
 
 %% Remove >20 kHz
 % sounds muffled
-fft1_half = floor(length(fft1)/2);
+fft1Mid = floor(length(fft1)/2);
 LPFcutoff = 20000 * N1/fs;
-fft1_LPF = fft1;
-fft1_LPF(LPFcutoff+2:fft1_half) = 0;
-fft1_LPF(fft1_half:end-LPFcutoff) = 0;
-lpf1_out = ifft(fft1_LPF);
+fft1LPF = fft1;
+fft1LPF(LPFcutoff+2:fft1Mid) = 0;
+fft1LPF(fft1Mid:end-LPFcutoff) = 0;
+lpf1_out = ifft(fft1LPF);
 % sound(seg1(1:10*fs), fs);
 % pause();
 % sound(lpf1_out(1:10*fs), fs);
-figure(4); plot(omega1khz, fftshiftnormdb(fft1_LPF)); title("lpf");
+figure(2); plot(omega1khz, fftshiftNormdB(fft1LPF)); title("lpf");
 
 %% Quantization of low power elements
-fft1_quant = fft1;
+fft1Quant = fft1;
 threshold = 0.00001 * max(abs(fft1));
-low_index = abs(fft1) < threshold;
-fft1_quant(low_index) = 0;
+lowIndex = abs(fft1) < threshold;
+fft1Quant(lowIndex) = 0;
 
-figure(2); plot(omega1khz, fftshiftnormdb(fft1_quant)); title("quantised");
-seg1_out = ifft(fft1_quant);
-error = (seg1_out - seg1);
-figure(3)
+figure(3); plot(omega1khz, fftshiftNormdB(fft1Quant)); title("quantised");
+seg1Out = ifft(fft1Quant);
+error = (seg1Out - seg1);
+figure(4)
 plot((1:length(error(160:end-160)))/fs,error(160:end-160));
 title( ...
     sprintf("Error in time domain of removing frequencies\nwith power under %.3f%% of max power", ...
@@ -65,28 +65,28 @@ xlabel("Seconds")
 ylabel("Absolute Error")
 % sound(seg1_out(1:fs*5), fs);
 
-%% Flat thresholds
+%% Static thresholds
 thresholds = [0.000001:0.000001:0.00005-0.000001,0.00005:0.00001:0.001];
 prev = zeros(1,length(fft1));
 errors = zeros(1,length(thresholds));
-difference = zeros(1,length(thresholds));
-num_coeff_deleted = zeros(1,length(thresholds));
+coeffDiff = zeros(1,length(thresholds));
+numCoeffDeleted = zeros(1,length(thresholds));
 for i = 1:length(thresholds)
-    fft1_quant = fft1;
+    fft1Quant = fft1;
     threshold = thresholds(i) * max(abs(fft1));
-    low_index = abs(fft1) < threshold;
-    fft1_quant(low_index) = 0;
+    lowIndex = abs(fft1) < threshold;
+    fft1Quant(lowIndex) = 0;
 
     % ifft & error
-    seg1_out = ifft(fft1_quant);
-    error = (seg1_out - seg1);
+    seg1Out = ifft(fft1Quant);
+    error = (seg1Out - seg1);
     % Ignore 1st/last 160 values as there are edge artefacts
     % av_error = mean(abs(error(160:end-160)));
-    ss_error = sum(error(160:end-160).^2);
-    errors(i) = ss_error;
-    difference(i) = sum(low_index-prev);
-    prev = low_index;
-    num_coeff_deleted(i) = sum(low_index);
+    ssError = sum(error(160:end-160).^2);
+    errors(i) = ssError;
+    coeffDiff(i) = sum(lowIndex-prev);
+    prev = lowIndex;
+    numCoeffDeleted(i) = sum(lowIndex);
     % errors(floor(i/0.00005)+1) = av_error;
 end
 
@@ -96,63 +96,92 @@ title("Normalised Error by Threshold Level")
 xlabel("Lowest magnitude kept")
 ylabel("Normalised Error (dB)")
 
-figure(11);
-plot(num_coeff_deleted, 20*log10(abs(errors)))
+figure(6);
+plot(numCoeffDeleted, 20*log10(abs(errors)))
 title("Normalised Error by Number of Coefficients Deleted")
 xlabel("Number of Coefficients Deleted")
 ylabel("Normalised Error (dB)")
 
-%%
-figure(9);
-plot(thresholds*100,difference);
+figure(7);
+plot(thresholds*100,coeffDiff);
 title("Number of new coefficients deleted at current threshold compared to previous threshold")
 xlabel("Thresholds (% of max magnitude of power)")
 ylabel("No. of new coeff deleted")
 
-%% test the jump of error
-fft1_quant = fft1;
+%% test a threshold
+fft1Quant = fft1;
 threshold = 0.0001 * max(abs(fft1));
-low_index62 = abs(fft1) < threshold;
-fft1_quant(low_index62) = 0;
-seg1_out = ifft(fft1_quant);
-error62 = (seg1_out - seg1);
-ss_error = mean(abs(error62(160:end-160)))
-sound(seg1_out(1:7*fs), fs);
+lowIndex62 = abs(fft1) < threshold;
+fft1Quant(lowIndex62) = 0;
+seg1Out = ifft(fft1Quant);
+error62 = (seg1Out - seg1);
+ssError = mean(abs(error62(160:end-160)))
+sound(seg1Out(1:7*fs), fs);
 % pause();
-figure(6);
-plot(omega1khz, fftshiftnormdb(fft1_quant));
+figure(8);
+plot(omega1khz, fftshiftNormdB(fft1Quant));
 title("threshold = 0.00062");
-temp1 = fft1_quant;
+temp1 = fft1Quant;
 
+%% Dynamic Thresholds
+% Base thresholds around peaks
 
-fft1_quant = fft1;
-threshold = 0.00063 * max(abs(fft1));
-low_index63 = abs(fft1) < threshold;
-fft1_quant(low_index63) = 0;
-seg1_out = ifft(fft1_quant);
-error63 = (seg1_out - seg1);
-ss_error = mean(abs(error63(160:end-160)))
-% sound(seg1_out(1:7*fs), fs);
+% Identify peaks in the original FFT (only in positive frequencies, just
+% replicate it for negative frequencies later)
+fft1Mid = floor(length(fft1)/2);
+fft1Half = fft1(1:fft1Mid+1);
+fft1dBHalf = 20*log10(abs(fft1Half)+1e-12);
+[peaks, locs] = findpeaks(fft1dBHalf, 'MinPeakHeight', 10);
 
-figure(7);
-plot(omega1khz, fftshiftnormdb(fft1_quant));
-title("threshold = 0.00063")
-temp2 = fft1_quant;
+% Define rate of decay of masking constants
+upwardsDecay = 0.5; % Masking decays slower upwards
+downwardsDecay = 2;
+
+% Calculate dynamic thresholds based on peak and their locations
+dynThresholds = -inf(size(fft1dBHalf));
+for i = 1:length(locs)
+    baseloc = locs(i);
+    
+    for bin = 1:fft1Mid+1
+        displacement = bin - baseloc;
+
+        % Calculate decay based on distance. Higher distance = less masking
+        if (displacement > 0)
+            decay = displacement * upwardsDecay;
+        else 
+            decay = -displacement * downwardsDecay;
+        end
+        
+        % Use the least overall decay/highest threshold (i.e. apply the
+        % most masking)
+        dynThresholds(bin) = max(dynThresholds(bin), peaks(i) - decay);
+    end
+end
+
+%% Apply dynamic thresholds to the FFT. Still simply cut off the frequency,
+% but at least it is according to nearby frequencies
+fft1dBHalf(fft1dBHalf < dynThresholds) = -Inf;
+fft1Half(fft1dBHalf < dynThresholds) = 0;
+
+% Flip and add in the negative frequencies
+fft1dBFull = [fft1dBHalf, flip(fft1dBHalf(2:end-1))];
+fft1Full = [fft1Half, conj(flip(fft1Half(2:end-1)))];
+
+seg1Out = ifft(fft1Full);
+sound(seg1Out(1:7*fs), fs);
 
 %%
-figure(8);
-% temp1(isinf(temp1)) = 0;
-% temp2(isinf(temp2)) = 0;
-temp3 = temp1 - temp2;
-temp3 = fftshiftnormdb(temp3);
-% temp3(isinf(temp3))=0;
+figure(9);
+% plot(omega1khz, fftshiftNormdB(10.^(fft1dBFull./20)));
+% title("dynamic threshold");
+% 
+% seg1Out = ifft(10.^(fft1dBFull./20));
+% sound(seg1Out(1:7*fs), fs);
 
-% plot(omega1khz, abs(temp3)/max(abs(temp3)));
+plot(omega1khz, fftshiftNormdB(fft1Full));
+title("dynamic threshold");
 
-% plot errors in subplot
-subplot(2,1,1);
-plot(error62(160:end-160));
-title("error62");
-subplot(2,1,2);
-plot(error63(160:end-160));
-title("error63");
+seg1Out = ifft(fft1Full);
+sound(seg1Out(1:7*fs), fs);
+
+all(fftshiftNormdB(10.^(fft1dBFull./20))==fftshiftNormdB(fft1Full))
