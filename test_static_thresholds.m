01fc14890ded781b4662030a0c48639fd9c51629
Jeremy <jlim0208@student.monash.edu>
2026-05-08 22:42:48 +1000

    Simplify code
     - Uses size rather than explicitly forcing shape

--- test_static_thresholds.m
+++ test_static_thresholds.m
@@ -1,12 +1,12 @@
 %% Test multiple thresholds
 % Test different threshold levels using static_masking
-function [errors, coeffDiff, numCoeffDel] = test_static_thresholds(fft, thresholds, ogSignal)
+function [errors, coeffDiff, numCoeffDel] = test_static_thresholds(fft, thresholds, ogSignal, )
     % Preallocate
-    prev = zeros(1,length(fft)); % Save previous coefficients deleted to 
-                                  % calculate difference
-    errors      = zeros(1, length(thresholds));
-    coeffDiff   = zeros(1, length(thresholds));
-    numCoeffDel = zeros(1, length(thresholds));
+    prev = zeros(size(fft)); % Save previous coefficients deleted to 
+                             % calculate difference
+    errors      = zeros(size(thresholds));
+    coeffDiff   = zeros(size(thresholds));
+    numCoeffDel = zeros(size(thresholds));
     
     % Main testing loop
     for i = 1:length(thresholds)
